036e0a026fd8784d921859ad1acc7850db08aed0
Youssef Sherif <sharief@aucegypt.edu>
2018-08-29 10:52:59 +0200

    Clean code

--- functions/intents/display_meetup_intents.js
+++ functions/intents/display_meetup_intents.js
@@ -1,100 +1,69 @@
 const rquestApi = require('request-promise')
 const apiKey = require('../config/config').meetupAPIKey
 const stateStore = require('../state_store')
-const responses = require('../responses/response')
-const constants = require('../config/constants')
+// const responses = require('../responses/response')
+// const constants = require('../config/constants')
 const colors = require('colors/safe');
+const image = 'https://secure.meetupstatic.com/s/img/786824251364989575000/logo/swarm/m_swarm_630x630.png'
 
-function buildSingleMeetupResponse() {
-	let responseToUser, displayText, endConversation
-	const image = 'https://secure.meetupstatic.com/s/img/786824251364989575000/logo/swarm/m_swarm_630x630.png'
-	if (!stateStore.app.data.meetupData.length) {
-		endConversation = true
-		responseToUser = 'No meetups available now'
-		displayText = 'No meetups available now'
+function buildSingleMeetupResponse(events) {
+	if (!events.length) {
+		stateStore.app.tell('No meetups available now')
 	} else if (stateStore.app.data.meetupCount < 0) {
-		endConversation = true
-		responseToUser = 'No more meetups before this one'
-		displayText = 'No more meetups before this one'
-	} else if (stateStore.app.data.meetupCount < stateStore.app.data.meetupData.length) {
-		let meetup = stateStore.app.data.meetupData[stateStore.app.data.meetupCount]
+		stateStore.app.tell('No more meetups before this one')
+	} else if (stateStore.app.data.meetupCount < events.length) {
+		let meetup = events[stateStore.app.data.meetupCount]
 		const date = new Date(meetup.time)
 		let displayText = `Meetup number ${stateStore.app.data.meetupCount + 1} ${meetup.name} by ${meetup.group.name} on ${date.toDateString()}`
 		let speech = `<speak> 
-		<say-as interpret-as="ordinal"> ${stateStore.app.data.meetupCount+1} </say-as> meetup. Is ${meetup.name}. <break time="1"/> by ${meetup.group.name}
-		<break time="1"/> on ${date.toDateString()}. <break time="1600ms"/> For more visit website <break time="800ms"/>. Say next meetup for more
+		<say-as interpret-as="ordinal"> ${stateStore.app.data.meetupCount + 1} </say-as> meetup is ${meetup.name}. <break time="1"/> by ${meetup.group.name}
+		<break time="1"/> on ${date.toDateString()}. <break time="1600ms"/> For more, visit website <break time="800ms"/>. Say next meetup for more
 		`
-		
-		let googleRichResponse = stateStore.app.buildRichResponse().addSimpleResponse(displayText).addBasicCard(
+		let res = stateStore.app.buildRichResponse().addSimpleResponse({speech, text: displayText}).addBasicCard(
 			stateStore.app.buildBasicCard(meetup.description).setTitle(meetup.name).addButton(`Read more ${meetup.link}`)
 				.setImage(image, meetup.name).setImageDisplay('CROPPED')
 		)
-		responseToUser = {
-			googleRichResponse, displayText, speech
-		}
+		stateStore.app.ask(res)
 	} else {
-		responseToUser = 'There are no more meetups schedules at this time'
-		displayText = 'There are no more meetups schedules at this time'
-	}
-
-	if (stateStore.requestSource === constants.googleAssistantRequest) {
-		console.log(colors.yellow('yes google'))
-		return endConversation ? stateStore.app.tell(responseToUser.googleRichResponse) : stateStore.app.askWithList(displayText, responseToUser.googleListResponse)
-		// responses.sendGoogleResponse(responseToUser, endConversation)
-	} else {
-		responses.sendResponse(displayText)
+		stateStore.app.tell('There are no more meetups schedules at this time')
 	}
 }
 
-function buildMeetupListResponse() {
-	let responseToUser, displayText, endConversation
-	const image = 'https://secure.meetupstatic.com/s/img/786824251364989575000/logo/swarm/m_swarm_630x630.png'
-	if (!stateStore.app.data.meetupData.length) {
-		endConversation = true
-		displayText = 'No meetups available now'
-	} else if (stateStore.app.data.meetupCount < stateStore.app.data.meetupData.length) {
-		displayText = 'This is a list of meetups. Please select one of them to proceeed. '
-		let speech = `<speak> 
-		This is a list of meetups <break time="800ms"/>.
-		</speak>
-		`
+function buildMeetupListResponse(events) {
+	if (!events.length) {
+		stateStore.app.tell('No meetups available now')
+	} else if (stateStore.app.data.meetupCount < events.length) {
+		// let displayText = events.reduce((prev, meetup, i) =>
+		// 	prev + `Meetup number ${i + 1} : ${meetup.name} by ${meetup.group.name} on ${new Date(meetup.time).toDateString()}, For more information, 
+		// 	type meetup number ${i + 1}.\n`, 'This is a list of foods ')
 
-		let googleListResponse = stateStore.app.buildList('List of meetups')
-		stateStore.app.data.meetupData.forEach((meetup, i) => {
-			const date = new Date(meetup.time)
-			googleListResponse.addItems(
+		let speech = events.reduce((prev, meetup, i) => i < 1 ?
+			prev + `<say-as interpret-as="ordinal"> ${i + 1} <say-as> meetup <break time="500ms"/> is ${meetup.name}
+				<break time="700ms"/> on ${new Date(meetup.time).toDateString()}. For more information say "meetup ${i + 1}"  <break time="1200ms"/>`
+			: '', `<speak> 
+			This is a list of meeaeetups <break time="800ms"/>.
+			</speak>
+			`)
+
+		let res = stateStore.app.buildRichResponse().addSimpleResponse(speech).buildList('List of meeaaatups')
+		events.forEach((meetup, i) => {
+			res.addItems(
 				stateStore.app.buildOptionItem(`meetup ${i}`).setTitle(`meetup ${i + 1} :`).setImage(image, meetup.name)
 			)
-			displayText += `Meetup number ${i + 1} : ${meetup.name} by ${meetup.group.name} on ${date.toDateString()}, For more information, 
-			type meetup number ${i + 1}.`
-
-			if(i<1) {
-				speech =+ `<say-as interpret-as="ordinal"> ${i+1} <say-as> meetup <break time="500ms"/> is ${meetup.name}
-				<break time="700ms"/> on ${date.toDateString()}. For more information say "meetup ${i+1}"  <break time="1200ms"/>`
-			}
 		});
-
-		responseToUser = {
-			googleListResponse: googleListResponse, speech, displayText
-		}
+		stateStore.app.ask(res)
 	} else {
-		displayText = 'There are no more meetups schedules at this time'
-	}
-
-	if (stateStore.requestSource === constants.googleAssistantRequest) {
-		responses.sendGoogleResponse(responseToUser, endConversation)
-	} else {
-		responses.sendResponse(displayText)
+		stateStore.app.ask('There are no more meetups schedules at this time')
 	}
 }
 
 
 function getMeetupData() {
-	return rquestApi(`https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=31.360367&page=20&lat=30.046225&key=${apiKey}`).then(data => {
+	return rquestApi(`https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=31.360367&lat=30.046225&key=${apiKey}`).then(data => {
 		const meetups = JSON.parse(data)
+		console.log(colors.yellow(meetups.events))
 		if (meetups.events) {
-			console.log(colors.yellow('yessssss'))
-			stateStore.app.data.meetupData = meetups.events
+			return meetups.events
 		}
 	}).catch(err => {
 		console.log(colors.red(err))
@@ -102,19 +71,15 @@ function getMeetupData() {
 }
 
 function displayMeetup() {
-	// if (stateStore.app.data.meetupData.length === 0) {
-	getMeetupData().then(() => {
-		buildSingleMeetupResponse()
+	return getMeetupData().then((events) => {
+		buildSingleMeetupResponse(events)
 	})
-	// }
 }
 
 function displayMeetupList() {
-	// if (stateStore.app.data.meetupData.length === 0) {
-	getMeetupData().then(() => {
-		buildMeetupListResponse()
+	return getMeetupData().then((events) => {
+		buildMeetupListResponse(events)
 	})
-	// }
 }
 
 module.exports = {
@@ -136,9 +101,7 @@ module.exports = {
 		displayMeetupList()
 	},
 	showNumber() {
-		console.log(colors.yellow('showing number'))
 		let param = stateStore.app.getContextArgument('actions_intent_option', 'OPTION')
-		console.log(colors.yellow('param', param))
 		if (param) {
 			stateStore.app.data.meetupCount = parseInt(param.value.replace('meetup ', ''))
 		}
@@ -147,6 +110,5 @@ module.exports = {
 			stateStore.app.data.meetupCount = number[0] - 1
 		}
 		displayMeetup()
-		console.log(colors.yellow('number', number))
 	}
 }
--- functions/intents/other_intents.js
+++ functions/intents/other_intents.js
@@ -4,19 +4,22 @@ const constants = require('../config/constants')
 
 module.exports = {
     panic() {
-        return stateStore.requestSource === constants.googleAssistantRequest ? responses.sendGoogleResponse(`
-            <speak> 
-            Step 1 take a deep breath. \n <break time="2600ms"/>
-            Step 2 Exhale.\n 
-            Step 3 take a deep breath again.\n  <break strength="weak"/>
-            Step 4 exhale.\n 
-            </speak>
-        `) : responses.sendResponse(`
+        const displayText = `
         Step 1 take a deep breath. \n
         Step 2 Exhale \n
         Step 3 take a deep breath again \n
         Step 4 exhale \n
-        `)
+        `
+        const speech = `
+        <speak> 
+        Step 1 take a deep breath. \n <break time="2600ms"/>
+        Step 2 Exhale.\n 
+        Step 3 take a deep breath again.\n  <break strength="weak"/>
+        Step 4 exhale.\n 
+        </speak>
+    `
+        return stateStore.app.tell({ displayText, speech })
+        // return stateStore.requestSource === constants.googleAssistantRequest ? responses.sendGoogleResponse() : responses.sendResponse()
     },
 
 }
